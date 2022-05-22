7363000d34ea72cab016b6a5cc313194a0e74000
rishirajpal01 <happy.rajpal2001@gmail.com>
2022-05-22 18:21:02 +0530

    Study Plan Day 2

---
+++ number_of_1_bits.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+int hammingWeight(uint32_t n) {
+        int count = 0;
+        while (n){
+            n &= (n-1);
+            count++;
+        }
+        return count;        
+}
+
+int main(){
+    uint32_t n = 00000000000000000000000000001011;
+    std::cout<<hammingWeight(n);
+    return 0;
+}
---
+++ subtract_product_&_sum_of_digits.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+int subtractProductAndSum(int n) {
+        int product=1, sum =0;
+        while (n>0) {
+            product *= (n%10);
+            sum += (n%10);
+            n /= 10;
+        }
+        return (product-sum);
+}
+
+int main(){
+    int input = 123;
+    std::cout<<subtractProductAndSum(input);
+    return 0;
+}
