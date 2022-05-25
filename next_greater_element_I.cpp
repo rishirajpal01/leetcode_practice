1416bfd564c93f24fa911251b309d14d51f91c74
rishirajpal01 <happy.rajpal2001@gmail.com>
2022-05-25 19:07:40 +0530

    Study Plan Day 5

--- next_greater_element_I.cpp
+++ next_greater_element_I.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> ans;
-        stack<int> st;
-        int n = nums2.size();
-        
-        for (int i = n - 1; i >= 0; --i) {
-            int num = nums2[i];
-            while (!st.empty() && st.top() <= num)
-                st.pop();
-            if (!st.empty())
-                ans[num] = st.top();
-            st.push(num);
+        stack<int> s;
+        unordered_map<int, int> m;
+        for (int n : nums2) {
+            while (s.size() && s.top() < n) {
+                m[s.top()] = n;
+                s.pop();
+            }
+            s.push(n);
         }
-        
-        vector<int> out;
-        for (int num : nums1) {
-            if (ans.count(num))
-                out.push_back(ans[num]);
-            else
-                out.push_back(-1);
-        }
-        return out;
+        vector<int> ans;
+        for (int n : nums1) 
+            ans.push_back(m.count(n) ? m[n] : -1);
+        return ans;
     }
 };
